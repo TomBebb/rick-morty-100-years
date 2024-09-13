b30fe4e2e2998648b9836510849493c5088bbd86
Tom Bebbington <tombebb@protonmail.com>
2024-09-13 16:07:10 +0100

    Display gender icon

--- src/components/CharacterDisplay.vue
+++ src/components/CharacterDisplay.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-import { Character } from '../models'
-
+import { Character, CharacterGender } from '../models'
+import { Icon } from '@iconify/vue'
+const iconByGender: Record<CharacterGender, string> = {
+  Female: 'mdi:gender-female',
+  Male: 'mdi:gender-male',
+  Genderless: 'mdi:human-genderless',
+  unknown: 'mdi:question',
+}
 defineProps<{ data: Character }>()
 </script>
 <template>
   <div class="flex flex-col items-center gap-2">
-    <div class="font-bold">{{ data.name }}</div>
+    <div class="flex flex-row items-center gap-2 font-bold">
+      <Icon :icon="iconByGender[data.gender]" />
+
+      {{ data.name }}
+    </div>
     <div>{{ data.species }}</div>
-    <div>{{ data.gender }}</div>
+    <div>{{ data.location.name }}</div>
+    <div>{{ data.origin.name }}</div>
   </div>
 </template>
