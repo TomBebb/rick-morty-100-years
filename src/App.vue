a423eb1b35ff1da781530a0e8269646a3a26111b
Tom Bebbington <tombebb@protonmail.com>
2024-09-25 19:09:43 +0100

    Remove unused import

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import MyButton from './components/MyButton.vue'
 import NavBar from './components/NavBar.vue'
 const curr = useRoute()
 </script>
