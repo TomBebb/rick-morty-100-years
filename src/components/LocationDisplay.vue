16343ae09a1db6f58f5622678efbe661aaca2d06
Tom Bebbington <tombebb@protonmail.com>
2024-10-07 22:11:09 +0100

    Add basic locations table

---
+++ src/components/LocationDisplay.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Location, ColorKind } from '../models'
+
+defineProps<{ data: Location }>()
+</script>
+<template>
+  <div class="flex flex-col items-center gap-2">
+    <div class="font-bold">
+      {{ data.name }}
+    </div>
+    <div>{{ data.dimension }}</div>
+    <div>{{ data.type }}</div>
+  </div>
+</template>
